7b83da6c9f89fb3250c2598c659256bd1b19e8c1
Sarina Naeimi <snnaeimi@gmail.com>
2024-10-20 16:49:15 -0700

    about and footer part with tailwind and react

--- src/components/AboutAndInterest.js
+++ src/components/AboutAndInterest.js
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function AboutAndInterest() {
-  return <h1>About And Interest component</h1>;
+  return (
+    <div className="flex flex-col text-left ml-9 mt-6">
+      <div className="mb-7">
+        <h1 className="text-xl font-bold text-[#2B283A] mb-[5px]">About</h1>
+        <p className="text-[#4A4E74] text-xs">
+          I am a frontend developer with a particular interest in making things
+          simple and automating daily tasks. I try to keep up with security and
+          best practices, and am always looking for new things to learn.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h1 className="text-xl font-bold text-[#2B283A] mb-[5px]">Interests</h1>
+        <p className="text-[#4A4E74] text-xs">
+          Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+          Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/components/SocialButtons.js
+++ src/components/SocialButtons.js
@@ -1,5 +1,20 @@
 import React from "react";
+import {
+  FaTwitterSquare,
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaGithubSquare
+} from "react-icons/fa";
 
 export default function SocialButtons() {
-  return <h1>Social Buttons component</h1>;
+  return (
+    <div className="flex justify-center text-2xl text-[#918E9B]">
+      <FaTwitterSquare className="mr-6" />
+      <FaFacebookSquare className="mr-6" />
+      <FaInstagramSquare className="mr-6" />
+      <FaLinkedin className="mr-6" />
+      <FaGithubSquare />
+    </div>
+  );
 }
